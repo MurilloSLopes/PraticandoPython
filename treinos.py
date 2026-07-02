b177cba22c49e1cea60c257e5818ed3b388cd436
murilloslopes <murillosivierolopes2310@gmail.com>
2026-07-02 20:02:01 -0300

    Adiciona primeiros arquivos Python.

---
+++ treinos.py
@@ -0,0 +1,13 @@
+
+print('Python na Escola de Programação da Alura.\n')
+
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+print(f'Meu nome é {nome} e tenho {idade} anos.\n') 
+
+print('A','L','U','R','A', sep='\n') # Imprime cada letra em uma linha separada 
+
+
+pi = 3.14159
+print(f'O valor arredondade de pi é {pi:.2f}\n')  # Arredonda para 2 casas decimais
+
