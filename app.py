023da3f98b15627352b162233ba0a5b2fc5d8e56
murilloslopes <murillosivierolopes2310@gmail.com>
2026-07-03 17:24:18 -0300

    feat: criacao da funcao encerrar app

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 
 print ('''
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -13,7 +15,11 @@ print ('2. Listar restaurantes')
 print ('3. Ativar restaurantes')
 print ('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
+opcao_escolhida = int(input('Escolha uma opção: ')) #criacao de variavel
+
+def finalizar_app(): #def usando para definir uma função
+    os.system('cls') #os.system cls esta pegando a biblioteca os (importada na primeira linha) e ativando o comando para limpar o terminal
+    print('Encerrando app\n')
 
 if opcao_escolhida == 1:
     print('Cadastrar restaurantes')
@@ -22,4 +28,4 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:     
     print('Ativar restaurantes')
 else:
-    print('Encerrando programa')
+    finalizar_app()
