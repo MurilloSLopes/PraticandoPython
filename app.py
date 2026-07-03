5f2a72e5a829b7d0161fbbdcdf2999cfae83ce4e
murilloslopes <murillosivierolopes2310@gmail.com>
2026-07-03 17:40:19 -0300

    feat: funcoes organizadas no arquivo app.py e inclusao do um novo exercicio no arquivo treinos.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 
-
-print ('''
+#def usando para definir uma função
+def exibir_nome_do_programa():
+    print ('''
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -10,22 +11,36 @@ print ('''
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░ 
        ''')
 
-print ('1. Cadastrar restaurantes')
-print ('2. Listar restaurantes')
-print ('3. Ativar restaurantes')
-print ('4. Sair\n')
+def exibir_opcoes():
+    print ('1. Cadastrar restaurantes')
+    print ('2. Listar restaurantes')
+    print ('3. Ativar restaurantes')
+    print ('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opção: ')) #criacao de variavel
-
-def finalizar_app(): #def usando para definir uma função
+def finalizar_app():
     os.system('cls') #os.system cls esta pegando a biblioteca os (importada na primeira linha) e ativando o comando para limpar o terminal
     print('Encerrando app\n')
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurantes')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:     
-    print('Ativar restaurantes')
-else:
-    finalizar_app()
+def escolher_opcao():    
+       opcao_escolhida = int(input('Escolha uma opção: ')) #criacao de variavel
+
+
+
+
+       if opcao_escolhida == 1:
+           print('Cadastrar restaurantes')
+       elif opcao_escolhida == 2:
+           print('Listar restaurantes')
+       elif opcao_escolhida == 3:     
+           print('Ativar restaurantes')
+       else:
+           finalizar_app()
+
+#bloco para definor o aquivo principal do app
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
--- treinos.py
+++ treinos.py
@@ -11,3 +11,13 @@ print('A','L','U','R','A', sep='\n') # Imprime cada letra em uma linha separada
 pi = 3.14159
 print(f'O valor arredondade de pi é {pi:.2f}\n')  # Arredonda para 2 casas decimais
 
+def classificar_musica(genero_favorito, genero_musica):
+    if genero_favorito == genero_musica:
+        return 'recomendada'
+    elif genero_favorito == 'Pop' or genero_favorito == 'Rock':
+        return 'neutra'
+    else:
+        return 'não recomendada'
+
+resultado = classificar_musica('Rock', 'Pop')
+print(resultado)
