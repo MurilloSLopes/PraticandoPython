5bd5b5e7dcee2c5cb63948e3007e5c70dd49393d
murilloslopes <murillosivierolopes2310@gmail.com>
2026-07-08 19:35:34 -0300

    feat: continuação de aula com a implementação de lacos for , dicionario e algumas variaveis de conceitos

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
 
+restaurantes = [{'nome':'Praca', 'categoria': 'Japonesa', 'ativo': False},
+                 {'nome': 'Pizza Suprema', 'categoria': 'Pizza', 'ativo': True},
+                 {'nome': 'Cantina', 'categoria': 'Italiano', 'ativo': False}] 
+
 #def usando para definir uma função
 def exibir_nome_do_programa():
     print ('''
@@ -18,26 +22,84 @@ def exibir_opcoes():
     print ('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls') #os.system cls esta pegando a biblioteca os (importada na primeira linha) e ativando o comando para limpar o terminal
-    print('Encerrando app\n')
+    exibir_subtitulo('Finalizando o app')
 
-def escolher_opcao():    
-       opcao_escolhida = int(input('Escolha uma opção: ')) #criacao de variavel
+def voltar_ao_menu_principal():
+    input('\ndigite uma tecla para voltar ao menu principal ')
+    main() #chamando a funcao main para voltar ao menu principal
+
+def opcao_invalida():
+    print('opção invalida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante: ')
+    categoria = input(f'Digite a categoria do restaurante: {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome': nome_do_restaurante, 'categoria': categoria, 'ativo': False}
+    restaurantes.append(dados_do_restaurante) #adicionando o nome do restaurante na lista de restaurantes
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
+    voltar_ao_menu_principal() #chamando a funcao voltar_ao_menu_principal para voltar ao menu principal
+
+def lista_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+    for restaurante in restaurantes: #for usando para percorrer a lista de restaurantes
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = restaurante['ativo']
+        print(f'- {nome_restaurante} | {categoria} | {ativo}')
+    voltar_ao_menu_principal() #chamando a funcao voltar_ao_menu_principal para voltar ao menu principal
+
+
+
+def alterar_estado_restaurante():
+    exibir_subtitulo('Alterando o estado do restauranre')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes: #for usando para percorrer a lista de restaurantes
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo'] #alterando o estado do restaurante usando o operador not
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso!' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso!'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print(f'O restaurante nao foi encontrado')
+        
+
+
+
+    voltar_ao_menu_principal
+
+def escolher_opcao():   
+    try: #try usando para "tentar" executar o bloco que esta abaixo.
+        opcao_escolhida = int(input('Escolha uma opção: ')) #criacao de variavel
 
 
 
 
-       if opcao_escolhida == 1:
-           print('Cadastrar restaurantes')
-       elif opcao_escolhida == 2:
-           print('Listar restaurantes')
-       elif opcao_escolhida == 3:     
-           print('Ativar restaurantes')
-       else:
-           finalizar_app()
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            lista_restaurantes()
+        elif opcao_escolhida == 3:     
+            alterar_estado_restaurante()
+        elif opcao_escolhida == 4:     
+            finalizar_app()
+        else:
+            opcao_invalida()
 
-#bloco para definor o aquivo principal do app
+    except:
+        opcao_invalida() #except usando para caso o try nao funcione ele vai executar tal funcao
+
+#bloco para definir o aquivo principal do app
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
--- treinos.py
+++ treinos.py
@@ -47,3 +47,72 @@ if usuario == usuario_correto and senha == senha_correta:
     print("Login bem sucedido!")
 else:
     print("Credenciais inválidas. Tente novamente.")
+
+# Criando uma lista de compras
+lista_de_compras = ["Maçã", "Banana", "Leite", "Pão", "Queijo"]
+
+# Adicionando um item à lista
+lista_de_compras.append("Ovos")
+
+# Removendo um item da lista
+lista_de_compras.remove("Banana")
+
+# Exibindo a lista
+print("Lista de Compras:")
+for item in lista_de_compras:
+    print("- " + item)
+
+#Exercícios
+##1 - Crie uma lista para cada informação a seguir:
+#Lista de números de 1 a 10;
+#Lista com quatro nomes;
+#Lista com o ano que você nasceu e o ano atual.
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+nomes = ["Alice", "Bob", "Charlie", "David"]
+anos = [1999, 2026]
+
+# 2 - Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+for numero in numeros:
+    print(numero)
+
+# 3 - Utilize um loop for para calcular a soma dos números ímpares de 1 a 10.
+soma_impares = 0
+for i in range(1, 11,2): # O range(1, 11, 2) gera os números ímpares de 1 a 10
+    soma_impares += i # Adiciona o número ímpar à soma
+    print(soma_impares)
+
+#4 - Utilize um loop for para imprimir os números de 1 a 10 em ordem decrescente.
+for i in range(10, 0, -1):
+    print(i)
+
+# 5 - Solicite ao usuário um número e, em seguida, utilize um loop for para imprimir a tabuada desse número, indo de 1 a 10.
+numero_tabuada = int(input("Digite um número para a tabuada: "))
+for i in range(1, 11):
+    resultado = numero_tabuada * i
+    print(f"{numero_tabuada} x {i} = {resultado}")
+
+# 6 - Crie uma lista de números e utilize um loop for para calcular a soma de todos os elementos. Utilize um bloco try-except para lidar com possíveis exceções.
+lista_numeros = [10, 5, 8, 3, 7]
+soma = 0
+
+try:
+    for numero in lista_numeros:
+        soma += numero
+    print(f"Soma dos elementos: {soma}")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
+
+# 7 - Construa um código que calcule a média dos valores em uma lista. Utilize um bloco try-except para lidar com a divisão por zero, caso a lista esteja vazia.
+lista_valores = [15, 20, 25, 30]
+soma_valores = 0
+
+try:
+    for valor in lista_valores:
+        soma_valores += valor
+    media = soma_valores / len(lista_valores)
+    print(f"Média dos valores: {media}")
+except ZeroDivisionError:
+    print("A lista está vazia, não é possível calcular a média.")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
